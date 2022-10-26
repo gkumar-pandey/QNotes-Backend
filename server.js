05b7a9ab6e9b2ed459ed7ab4ed3e869c2fd24b52
gkumar-pandey <gautamshekhar078@gmail.com>
2022-10-27 00:28:10 +0530

    feat: port added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 const noteRoute = require("./Routes/noteRoutes.js");
 const userRouter = require("./Routes/userRoutes.js");
+require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
@@ -12,7 +13,7 @@ connectDB();
 app.use("/user", userRouter);
 app.use("/note", noteRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running at port : ${PORT}`);
 });
